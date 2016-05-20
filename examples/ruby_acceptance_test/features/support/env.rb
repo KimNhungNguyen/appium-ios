0ee2443dbda0a00f5706ca4f81730477cba316af
Nhung Nguyen <nnguyen@thoughtworks.com>
2016-05-20 10:26:54 +1000

    Update env.rb to run on Saucelab

--- examples/ruby_acceptance_test/features/support/env.rb
+++ examples/ruby_acceptance_test/features/support/env.rb
@@ -12,6 +12,9 @@ max_wait_in_seconds = 10
 World PageObject::PageFactory
 World Test::Unit::Assertions  # need this for 'assert' method
 
+SAUCE_USERNAME = ENV['nhung_nguyen']
+SAUCE_ACCESS_KEY = ENV['e6199f20-a2cb-436e-8158-51ad8bbbac6c']
+
 desired_caps = {
   	caps:       {
     	platformName:  'iOS',
@@ -24,6 +27,43 @@ desired_caps = {
 	    sauce_access_key:  nil
 	  }
 	}
+end
+
+def auth_details
+	un = SAUCE_USERNAME
+	pw = SAUCE_ACCESS_KEY
+	unless un && pw
+    STDERR.puts <<-EOF
+      Your SAUCE_USERNAME or SAUCE_ACCESS_KEY environment variables 
+      are empty or missing.
+      
+      You need to set these values to your Sauce Labs username and access
+      key, respectively.
+
+      If you don't have a Sauce Labs account, you can get one for free at
+      http://www.saucelabs.com/signup
+    EOF
+
+    exit
+  end
+
+  return "#{un}:#{pw}"
+end
+
+def server_url
+	"http://#{auth_details}@ondemand.saucelabs.com:80/wd/hub"
+end
+
+def rest_jobs_url
+  "https://#{auth_details}@saucelabs.com/rest/v1/#{SAUCE_USERNAME}/jobs"
+end
+
+# Because WebDriver doesn't have the concept of test failure, use the Sauce
+# Labs REST API to record job success or failure
+def update_job_success(job_id, success)
+  RestClient.put "#{rest_jobs_url}/#{job_id}", { 'passed' => success }.to_json, :content_type => :json
+end
+
 
 def start_appium
 	system "appium --log-level error &"
